294ccca7cc1e1ed31bed25d1d6a59dec2d6bde7c
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-03-27 22:30:16 +0900

    123
    123

--- Assets/Scripts/PlayerCont.cs
+++ Assets/Scripts/PlayerCont.cs
@@ -9,6 +9,7 @@ public class PlayerCont : MonoBehaviour
     public float jumpHeight = 0;
     public float jumpSpeed = 0;
     Vector2 startPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,30 +18,28 @@ public class PlayerCont : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
+    {   //점프키
+        if(Input.GetKeyDown(KeyCode.Space)){
             isJump = true;
-        }else if(transform.position.y <= startPosition.y)
-        {
+        } else if(transform.position.y <= startPosition.y){
             isJump = false;
             isTop = false;
             transform.position = startPosition;
         }
-        if(isJump) 
+        
+        if(isJump)
         {
-            if(transform.position.y <= jumpHeight - 0.1f&& !isTop)
-            {
-                transform.position = Vector2.Lerp(transform.position,new Vector2(transform.position.x, jumpHeight), jumpSpeed * Time.deltaTime);
-            }
+            if(transform.position.y <= jumpHeight-0.1f&&!isTop) {
+            transform.position = Vector2.Lerp(transform.position,new Vector2(transform.position.x, jumpHeight),jumpSpeed*Time.deltaTime);
+        } 
             else {
                 isTop = true;
             }
-            if(transform.position.y > startPosition.y && isTop)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, startPosition, jumpSpeed * Time.deltaTime);
-            }
-            }
-        
+        }
+        if(transform.position.y > startPosition.y && isTop)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,startPosition,jumpSpeed*Time.deltaTime);
+        }
+
     }
 }
