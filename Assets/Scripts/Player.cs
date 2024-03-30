c678995a14b6adafc468760daaa1507824a14c73
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-03-30 18:12:17 +0900

    Jump&Slide animator
    123

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+    public enum State { Run, Jump, Slide, Hit, Die, Skill }
     public float startJumpPower;
-    public float JumpPower;
     public float SlidePower;
     public bool isGround;
 
@@ -22,14 +22,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        //점프
+        if(Input.GetKeyDown(KeyCode.Space) && isGround) {
             rigid.AddForce(Vector2.up * startJumpPower, ForceMode2D.Impulse);
+            AnimatorChange(State.Jump);
         }
+        
+        //슬라이드
         if(Input.GetKeyDown(KeyCode.DownArrow)) {
-              Debug.Log("슬라이드 하는 중");
+              AnimatorChange(State.Slide);
         }
+        
     }
-
+    
+    void OnCollisionStay2D(Collision2D collision) {
+        
+        if(!isGround) {
+        AnimatorChange(State.Run); 
+        }
+       
+        isGround = true;
+    }   
+    void OnCollisionExit2D(Collision2D collision) {
+        AnimatorChange(State.Jump);
+        isGround = false;
+    }
+    //애니메이션 변경
+    void AnimatorChange(State state) {
+       animator.SetInteger("State", (int)state);
+    }
 
 
 
