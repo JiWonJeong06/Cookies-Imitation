cf1fb2d84b9c5285563321246f597cc20f56421a
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-03-31 18:19:49 +0900

    jump slide
    땅에 닿는 점프 버그가 있음
    
    머리 콩콩콩콩콩ㅇ콩

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,7 +10,7 @@ public class Player : MonoBehaviour
     public float startJumpPower;
     public bool isGround;
     public int JumpCount = 0;
-    public int MaxJumpCount = 1;
+    public int MaxJumpCount = 2;
     Rigidbody2D rigid;
     Animator animator;
     // Start is called before the first frame update
@@ -22,22 +23,18 @@ public class Player : MonoBehaviour
     {   
         //점프
         if(Input.GetKeyDown(KeyCode.Space) && (JumpCount < MaxJumpCount)) {
+            JumpCount++;
             rigid.velocity = Vector2.zero;
             rigid.AddForce(Vector2.up * startJumpPower, ForceMode2D.Impulse);
             AnimatorChange(State.Jump);
-            JumpCount++;
+            
         }
         
         //슬라이드
         if(Input.GetKey(KeyCode.DownArrow) && isGround) {
               AnimatorChange(State.Slide);
               
-              if(Input.GetKeyDown(KeyCode.Space) && (JumpCount < MaxJumpCount)) {
-                rigid.velocity = Vector2.zero;
-                rigid.AddForce(Vector2.up * startJumpPower, ForceMode2D.Impulse);
-                AnimatorChange(State.Jump);
-                JumpCount++;
-        }
+  
         }
         else if(Input.GetKeyUp(KeyCode.DownArrow)) {
               AnimatorChange(State.Run);
@@ -49,16 +46,16 @@ public class Player : MonoBehaviour
         
         if(!isGround) {
         AnimatorChange(State.Run); 
+        JumpCount = 0;
         }
         isGround = true;
-        JumpCount = 0;
+      
     }  
     // 점프 모션으로 변환
     void OnCollisionExit2D(Collision2D collision) {
         AnimatorChange(State.Jump);
         isGround = false;
     }
-
     //애니메이션 변경 함수
     void AnimatorChange(State state) {
        animator.SetInteger("State", (int)state);
