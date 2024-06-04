6acd28a9cd3f72f3293cd56723f4df4e48dc6815
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-06-04 22:23:10 +0900

    fix

--- Assets/Scripts/Infinity.cs
+++ Assets/Scripts/Infinity.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Infinity : MonoBehaviour
 {
     void LateUpdate() {
-        if (transform.position.x > -29) {
+        if (transform.position.x > -20) {
             return;
         }
-       transform.Translate(80,0,0, Space.Self); 
+       transform.Translate(70,0,0, Space.Self); 
         
     }
 }
