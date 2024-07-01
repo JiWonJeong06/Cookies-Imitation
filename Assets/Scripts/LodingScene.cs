77f4a701d98f191345a18427d8583f859ac47a07
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-07-01 16:52:43 +0900

    loac

--- Assets/Scripts/LodingScene.cs
+++ Assets/Scripts/LodingScene.cs
@@ -1,32 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-public class LodingScene : MonoBehaviour
+ 
+public class LoadingSceneManager : MonoBehaviour
 {
-    static string nextScene;
-
-    [SerializeField]
-    Image Bar;
-
-    public static void LodingScenes(string sceneName) {
-        nextScene = sceneName;
-        SceneManager.LoadScene("Loding Scene");
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(LoadScene());
-    }
-
-    IEnumerator LoadScene(){
-        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
-        op.allowSceneActivation = false;
-
-        while(!op.isDone){
-            yield return null;
-        }
-    }
+    public static string nextScene;
+ 
+    [SerializeField]
+    Image progressBar;
+ 
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+ 
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+ 
+    IEnumerator LoadScene()
+    {
+        yield return null;
+ 
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+ 
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            yield return null;
+ 
+            timer += Time.deltaTime;            if (op.progress < 0.9f)            {                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);                if (progressBar.fillAmount >= op.progress)                {                    timer = 0f;                }            }            else            {                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);                if (progressBar.fillAmount == 1.0f)                {                    op.allowSceneActivation = true;                    yield break;                }            }
+        }
+    }
 }
