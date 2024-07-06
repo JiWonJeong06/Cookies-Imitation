7a3a2506d8a251fa7105407f0bfdd231a8ad81b7
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-07-06 19:56:59 +0900

    cursor fix

--- Assets/Scripts/UI Scripts/NoCursor.cs
+++ Assets/Scripts/UI Scripts/NoCursor.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
+
 
 public class NoCursor : MonoBehaviour
+
 {
+    public GameObject defaultSelectedButton;
     // Use this for initialization
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        
+        
+    }
+    void Update(){
+      if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2) )
+        {
+            EventSystem.current.SetSelectedGameObject(defaultSelectedButton);
+        }
     }
 }
