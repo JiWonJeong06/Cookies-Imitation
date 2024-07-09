5779c6b5e10d14b99176267a826ecb52f670b5dd
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2024-07-09 17:29:28 +0900

    SoundSetting sc add

--- Assets/Scripts/SoundSetting.cs
+++ Assets/Scripts/SoundSetting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Setting : MonoBehaviour
+public class SoundSetting : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
